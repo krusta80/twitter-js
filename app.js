0d61431167088e5903103003e080ef54ef24494b
John Gruska <johngruska@Johns-MacBook-Pro-2.local>
2016-01-28 20:49:30 -0500

    3.Configuring Express

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var express = require( 'express' );
+var morgan = require('morgan');
 var app = express(); // creates an instance of an express application
 
 var port = 3000;
 
+
+
+app.use('/hello',function (req, res, next) {
+    // do your logging here
+    // call `next`, or else your app will be a black hole — receiving requests but never properly responding
+
+    console.log(req.method, req.url, res.statusCode);
+    next();
+})
+
+app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
